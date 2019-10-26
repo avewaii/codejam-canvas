09cc5041e37aaf553b8880b117a5f180836b72ca
avewaii <ewashadevsk@gmail.com>
2019-10-26 18:56:35 +0300

    feat: create basic html and css files

---
+++ 4x4.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementsById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+
+// [
+//     ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+//     ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+//     ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+//     ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+// ]
