bfdd9583ef87fdd311c79487bace6223f08e7303
avewaii <ewashadevsk@gmail.com>
2019-10-28 18:43:20 +0300

    docs: clone files from codejam-canvas to gh-pages

--- 4x4.js
+++ 4x4.js
@@ -1,12 +1,39 @@
-const canvas = document.getElementsById('canvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+canvas.style.backgroundColor="#c7c7c7";
 
-// [
-//     ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-//     ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-//     ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-//     ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-// ]
+const foXfo = document.getElementsByClassName('aside-menu__4x4')[0];
+
+// let data;
+
+let widthPixel; 
+
+let posPixelX = 0;
+let posPixelY = 0;
+
+foXfo.onclick = function(){
+    handler([
+        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+    ]);
+};
+
+function handler(data) {
+    for(let x = 0; x < data.length; x++){
+
+        for(let i = 0; i < data[x].length; i++){
+
+            widthPixel = 512/data[x].length;
+
+            ctx.fillStyle = "#" + data[x][i];
+            ctx.fillRect(posPixelX, posPixelY, widthPixel, widthPixel);
+            
+            posPixelX += widthPixel;
+        }
+        posPixelX = 0;
+        posPixelY += widthPixel; 
+    }    
+}
